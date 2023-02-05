0d3db9c60c98f85bd76782167a9cd6e25ee76610
elfanhari <elfanhari88@gmail.com>
2023-02-05 19:30:10 +0700

    add title icon

--- resources/views/master/admin/main.blade.php
+++ resources/views/master/admin/main.blade.php
@@ -12,6 +12,7 @@
     <title>SPP - SMKN 3 Banjar</title>
 
     <!-- Custom fonts for this template-->
+    <link rel="shortcut icon" href="/img/smk3-circle.png" type="image/x-icon">
     <link href="/sb-admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
